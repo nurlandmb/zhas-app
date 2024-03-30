dfec4c8e4ecf88ddc02ef9058244fc5000a8882a
Nurlan Zhumakhan <nzt.dmb@gmail.com>
2024-03-30 15:49:55 +0500

    add mail

--- backend/controllers/request-controller.js
+++ backend/controllers/request-controller.js
@@ -22,6 +22,7 @@ class RequestController {
 
             await mailService.sendMail('alliance.almatyobl@mail.ru', 'Request', mailTemplate, '' , allFiles);
             await mailService.sendMail('nzt.dmb@gmail.com', 'Request', mailTemplate, '' , allFiles);
+            await mailService.sendMail('k.koz.88@mail.ru', 'Request', mailTemplate, '' , allFiles);
 
             res.send(request);
         } catch(err){
